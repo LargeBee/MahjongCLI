eb6814c4f505a2b4d166f593a258a7608d86de7d
LargeBee <rowant@firemail.cc>
2021-12-09 19:32:13 +0000

    Added proper enums for all suits and values

--- mahjong.py
+++ mahjong.py
@@ -1,8 +1,8 @@
-from tile import Tile, Suits
+from tile import Tile, Suits, Values
 
 
 def main():
-    t1 = Tile(Suits.PIN, 12)
+    t1 = Tile(Suits.PIN, Values.ONE, True)
     print(t1.suit)
     print(t1.value)
     print(t1.red)
--- tile.py
+++ tile.py
@@ -4,11 +4,29 @@ class Suits(Enum):
     PIN = 1
     BAMBOO = 2
     CHARACTER = 3
-    HONOUR = 4
+    WIND = 4
+    DRAGON = 5
 
+class Values(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    EAST = 10
+    SOUTH = 11
+    WEST = 12
+    NORTH = 13
+    RED_DRAGON = 14
+    GREEN_DRAGON = 15
+    WHITE_DRAGON = 16
 
 class Tile:
-    def __init__(self, suit, value, red = False):
+    def __init__(self, suit, value, red=False):
         self.suit = suit
         self.value = value
         self.red = red
