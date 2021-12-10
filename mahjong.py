b055c49119dcd2eee38f944e70c8e5f10a087f78
LargeBee <rowant@firemail.cc>
2021-12-10 15:47:11 +0000

    Added clear function

--- mahjong.py
+++ mahjong.py
@@ -1,9 +1,13 @@
+import os
 from tile import Tile, Suits, Values
 from hand import Hand
 
+def clear():
+    os.system('cls||clear')
 
 def main():
     myHand = Hand()
+    clear()
     for handTile in myHand.generate_hand():
         print("{} of {}".format(handTile.value, handTile.suit))
 
